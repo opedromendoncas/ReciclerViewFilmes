36ca8480e107ce99a851add768b4835067e07e8d
opedromendoncas <pedoca.360@gmail.com>
2023-03-14 21:30:16 -0300

    atualização

--- app/src/main/java/br/com/etecia/reciclerviewfilmes/Filmes.java
+++ app/src/main/java/br/com/etecia/reciclerviewfilmes/Filmes.java
@@ -1,4 +1,51 @@
 package br.com.etecia.reciclerviewfilmes;
 
 public class Filmes {
+    private String Titulo;
+    private String Descricao;
+    private String Categoria;
+    private int imagem;
+
+    //criar construtor
+
+    public Filmes(String titulo, String descricao, String categoria, int imagem) {
+        Titulo = titulo;
+        Descricao = descricao;
+        Categoria = categoria;
+        this.imagem = imagem;
+    }
+
+    //criando os métodos de acesso get and setter
+
+    public String getTitulo() {
+        return Titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        Titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return Descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        Descricao = descricao;
+    }
+
+    public String getCategoria() {
+        return Categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        Categoria = categoria;
+    }
+
+    public int getImagem() {
+        return imagem;
+    }
+
+    public void setImagem(int imagem) {
+        this.imagem = imagem;
+    }
 }
